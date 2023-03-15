2a9975e26d14c46a0dcf52ec2e4eec67a7268347
Alessandro Scardova <alessandro@scardova.it>
2023-03-15 22:37:03 +0100

    Migrate to W11

--- InclusiveDemo/MainPage.xaml.cs
+++ InclusiveDemo/MainPage.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +27,7 @@ namespace InclusiveDemo
         public MainPage()
         {
             this.InitializeComponent();
+            CultureInfo.CurrentCulture = new CultureInfo("it-IT");
         }
     }
 }
